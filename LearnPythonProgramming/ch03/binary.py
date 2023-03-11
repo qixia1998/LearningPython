087a5a99cad98c388b3ebd6d61f5803db73c57bb
qixia1998 <qixia717138552@gmail.com>
2023-03-12 00:00:09 +0800

    add ch03

---
+++ LearnPythonProgramming/ch03/binary.py
@@ -0,0 +1,9 @@
+n = 39
+remainders = []
+while n > 0:
+    remainder = n % 2  # remainder of division by 2
+    remainders.append(remainder) # we keep track of remainders
+    n //= 2  # we divide n by 2
+
+remainders.reverse()
+print(remainders)
